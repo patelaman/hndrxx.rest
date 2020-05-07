f33d3f1a634e62586838084064fe91b1eabccf17
Aman <amanpatel2326@gmail.com>
2020-05-07 10:57:34 -0400

    Properly implement REST

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ addEventListener('fetch', event => {
 })
   
 async function handleRequest(request) {
+    const url = new URL(request.url);
     const quotes = JSON.parse(
-        await (await fetch('https://raw.githubusercontent.com/patelaman/hndrxx.rest/quotes/quotes.json')).text());
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    var header = request.headers.get('Content-Type')
-    if (header === 'application/json') {
-        return new Response(JSON.stringify({ quote: randomQuote }), {
-            headers: { 'Content-Type': 'application/json' },
-        })
-    } else {
-        return new Response(randomQuote, {
-            headers: { 'Content-Type': 'text/html' },
-        })
+        await (await fetch('https://raw.githubusercontent.com/patelaman/hndrxx.rest/quotes/quotes.json')).text())
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+
+    if (url.searchParams.get('format')) {
+        let formatParameter = url.searchParams.get('format').toLocaleLowerCase().split('/')[0]
+        if (formatParameter === 'text') {
+            return new Response(randomQuote, {
+                headers: { 'Content-Type': 'text/html' },
+            })
+        }
     }
+
+    return new Response(JSON.stringify({ quote: randomQuote }), {
+        headers: { 'Content-Type': 'application/json' },
+    })
 }
